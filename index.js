fe9c6e4230922e24ed74e05e324a0b975bcba3b6
MontY404 <93205559+CodeLost404@users.noreply.github.com>
2022-02-09 01:51:31 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true})
+
+
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === '!join') { 
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('** پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+
+} 
+
+} 
+
+});
+
+
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە lera Link Dane`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+    
+  client.channels.get("id Channel ").send(
+`> By: <@${message.author.id}> ✅ 
+${message.content}`)
+}
+    }
+        })
+
+
+
+
+client.on('ready', () => {
+console.log(`ON ${client.guilds.size} Servers`);
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Monty404 is here`) 
+});
+
+
+
+
+client.login("");//
+
+//Coded By Monty404
